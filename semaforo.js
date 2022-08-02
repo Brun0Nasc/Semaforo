85b425dbf47a10ca5cc5096dc6d83ddab4a24a00
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-08-02 08:13:14 -0300

    Update semaforo.js

--- semaforo.js
+++ semaforo.js
@@ -1,40 +1,41 @@
 var vm = document.getElementById("vm");
 var am = document.getElementById("am");
-var ve = document.getElementById("ve"); 
+var ve = document.getElementById("ve");
+var i = 0;
 
 function vermelho(){
     vm.style.backgroundColor="red";
     am.style.backgroundColor="rgb(131, 131, 131)";
     ve.style.backgroundColor="rgb(131, 131, 131)";
+    i = 3;
 }
 
 function amarelo(){
     am.style.backgroundColor="yellow";
     vm.style.backgroundColor="rgb(131, 131, 131)";
     ve.style.backgroundColor="rgb(131, 131, 131)";
+    i = 2;
 }
 
 function verde(){
     ve.style.backgroundColor="green";
     am.style.backgroundColor="rgb(131, 131, 131)";
     vm.style.backgroundColor="rgb(131, 131, 131)";
+    i = 1;
 }
 
-var i = 0;                
 
-function myLoop() {         
-  setTimeout(function() {
-    console.log('hello');
-    i++;
-    if (i == 1) {
-      vermelho();
-    } else if(i == 2){
-        amarelo();
-    } else{
-        verde();
-        i = 0;
-    }                 
-  }, 1000)
+
+setInterval(mudar, 3000);
+
+function mudar(){
+  if(i == 1){
+    verde();
+  } else if (i == 2){
+    amarelo();
+  } else {
+    vermelho();
+    i = 0
+  }
+  i++;
 }
-
-myLoop(); 
